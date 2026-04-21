d0e1b3ff3ea48c06846e14f6d572b23415cd62e2
Isabela Correa <sistemas@cib.org.co>
2026-04-21 08:04:38 -0500

    prueba mensajes1

--- models.py
+++ models.py
@@ -8,8 +8,6 @@ class Log(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     fecha_y_hora = db.Column(db.DateTime, default=datetime.utcnow)
     texto = db.Column(db.TEXT)
-def ordenar_por_fecha_y_hora(registros):
-    return sorted(registros, key=lambda x: x.fecha_y_hora,reverse=True)
 
 class Cita(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -23,10 +21,7 @@ class Cita(db.Model):
     estado = db.Column(db.String(20), default='pendiente')  # pendiente / confirmada / rechazada / cancelada
     creada_en = db.Column(db.DateTime, default=datetime.utcnow)
 
-
 class Consentimiento(db.Model):
-    __tablename__ = 'consentimientos'
-
     id = db.Column(db.Integer, primary_key=True)
     numero_whatsapp = db.Column(db.String(20))
     acepto = db.Column(db.Boolean)
@@ -42,7 +37,7 @@ class Asesor(db.Model):
         self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
-        return check_password_hash(self.password_hash, password)    
+        return check_password_hash(self.password_hash, password)
 
 def agregar_mensajes_log(texto):
     try:
